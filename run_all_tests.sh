e37df9299dbf732ca438fe05a241710bb0e448f7
Andy Spaven <=>
2019-12-04 15:32:50 +0000

    Force rebuild

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 export SBT_OPTS="-XX:+CMSClassUnloadingEnabled -XX:MaxMetaspaceSize=1G"
+
 sbt -Dlogger.application=INFO clean compile coverage test it:test component:test coverageOff coverageReport
+
