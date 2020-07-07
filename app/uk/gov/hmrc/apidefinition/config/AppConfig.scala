205119ba0f7e31e5662a5e8891202e202d779992
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2020-07-07 10:24:11 +0100

    APIS-4803 - Add facility to fetch wire model through document resources endpoints

--- app/uk/gov/hmrc/apidefinition/config/AppConfig.scala
+++ app/uk/gov/hmrc/apidefinition/config/AppConfig.scala
@@ -34,13 +34,15 @@ class AppConfig @Inject()(val runModeConfiguration: Configuration, environment: 
 
   val ramlLoaderRewrites = buildRamlLoaderRewrites
 
+  lazy val serviceBaseUrl = runModeConfiguration.getOptional[String]("serviceBaseUrl").getOrElse("http://localhost")
+
   def baseUrl(serviceName: String): String = {
     val context = runModeConfiguration.getOptional[String](s"$serviceName.context").getOrElse("")
 
     if (context.length > 0) s"${servicesConfig.baseUrl(serviceName)}/$context"
     else servicesConfig.baseUrl(serviceName)
   }
-  
+
    private def buildRamlLoaderRewrites: Map[String, String] = {
     Map(
       runModeConfiguration.getOptional[String](s"ramlLoaderUrlRewrite.from").getOrElse("") ->
