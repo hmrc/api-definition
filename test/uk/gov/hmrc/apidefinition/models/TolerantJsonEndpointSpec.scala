1fbe6cffb0d75490614903ce6462934ce88ee66f
John Green <john.green@digital.hmrc.gov.uk>
2023-11-02 13:51:55 +0000

    APIS-6516: Fix packages in tests

--- test/uk/gov/hmrc/apidefinition/models/TolerantJsonEndpointSpec.scala
+++ test/uk/gov/hmrc/apidefinition/models/TolerantJsonEndpointSpec.scala
@@ -14,11 +14,10 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.apiplatform.modules.apis.domain.models
+package uk.gov.hmrc.apidefinition.models
 
+import uk.gov.hmrc.apiplatform.modules.apis.domain.models._
 import uk.gov.hmrc.apiplatform.modules.common.utils.BaseJsonFormattersSpec
-
-import uk.gov.hmrc.apidefinition.models.TolerantJsonEndpoint
 
 class TolerantJsonEndpointSpec extends BaseJsonFormattersSpec {
 
