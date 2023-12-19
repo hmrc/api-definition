7da8911ba00debd51e1d6f096498d6df81629e71
jameshall1999 <67314983+jameshall1999@users.noreply.github.com>
2023-12-19 17:00:19 +0000

    API-7427 First test

--- app/uk/gov/hmrc/apidefinition/services/ApiRetirer.scala
+++ app/uk/gov/hmrc/apidefinition/services/ApiRetirer.scala
@@ -4,11 +4,20 @@ import uk.gov.hmrc.apidefinition.config.AppConfig
 import uk.gov.hmrc.apidefinition.utils.ApplicationLogger
 import javax.inject.Inject
 import scala.concurrent.Future
+import scala.concurrent.Future.successful
 import scala.concurrent.ExecutionContext
 
 class ApiRetirer @Inject() (config: AppConfig) extends ApplicationLogger {
   
-  def SayHello()(implicit ec: ExecutionContext): Future[String] = {
-    return  Future("hello")
+  def retireApis()(implicit ec: ExecutionContext): Future[String] = {
+    successful("hi")
+    // logger.info(s"Attempting to retire ${config.apisToRetire.length} APIs.")
+  //   Future.sequence(config.apisToRetire.map { api => findAndRetireApi(api) })
+  //     .map(_ => ())
+  // }
+
+  // private def findAndRetireApi(api: String)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Unit] = {
+  //   print("hi")
+  //   }
   }
 }
--- test/uk/gov/hmrc/apidefinition/service/ApiRetirerSpec.scala
+++ test/uk/gov/hmrc/apidefinition/service/ApiRetirerSpec.scala
@@ -3,6 +3,20 @@ import uk.gov.hmrc.apidefinition.config.AppConfig
 import uk.gov.hmrc.apidefinition.services.ApiRetirer
 import uk.gov.hmrc.apidefinition.repository.APIDefinitionRepository
 import scala.concurrent.ExecutionContext
+import uk.gov.hmrc.apiplatform.modules.apis.domain.models.StoredApiDefinition
+import scala.concurrent.Future.{failed, successful}
+import uk.gov.hmrc.apiplatform.modules.apis.domain.models.ServiceName
+import uk.gov.hmrc.apiplatform.modules.common.domain.models.ApiContext
+import uk.gov.hmrc.apiplatform.modules.apis.domain.models.ApiCategory
+import uk.gov.hmrc.apiplatform.modules.apis.domain.models.ApiVersion
+import uk.gov.hmrc.apiplatform.modules.common.domain.models.ApiVersionNbr
+import uk.gov.hmrc.apiplatform.modules.apis.domain.models.ApiStatus
+import uk.gov.hmrc.apiplatform.modules.apis.domain.models.ApiAccess
+import uk.gov.hmrc.apiplatform.modules.apis.domain.models.Endpoint
+import uk.gov.hmrc.apiplatform.modules.apis.domain.models.HttpMethod
+import uk.gov.hmrc.apiplatform.modules.apis.domain.models.AuthType
+import uk.gov.hmrc.apiplatform.modules.apis.domain.models.ResourceThrottlingTier
+
 
 class ApiRetirerSpec extends AsyncHmrcSpec {
 
@@ -14,14 +28,51 @@ class ApiRetirerSpec extends AsyncHmrcSpec {
     val underTest = new ApiRetirer(mockAppConfig)
   }
 
+  private val calendarApiVersion = ApiVersion(
+    versionNbr = ApiVersionNbr("1.0"),
+    status = ApiStatus.STABLE,
+    access = ApiAccess.PUBLIC,
+    endpoints = List(Endpoint("/date", "Check current date", HttpMethod.GET, AuthType.NONE, ResourceThrottlingTier.UNLIMITED))
+  )
+
+  private val calendarApiVersion2 = ApiVersion(
+    versionNbr = ApiVersionNbr("2.0"),
+    status = ApiStatus.STABLE,
+    access = ApiAccess.PUBLIC,
+    endpoints = List(Endpoint("/date", "Check current date", HttpMethod.GET, AuthType.NONE, ResourceThrottlingTier.UNLIMITED))
+  )
+
+  private val calendarApiVersion3 = ApiVersion(
+    versionNbr = ApiVersionNbr("3.0"),
+    status = ApiStatus.STABLE,
+    access = ApiAccess.PUBLIC,
+    endpoints = List(Endpoint("/date", "Check current date", HttpMethod.GET, AuthType.NONE, ResourceThrottlingTier.UNLIMITED))
+  )
+
+  private val calendarApiDefinition = StoredApiDefinition(
+    serviceName = ServiceName("calendar-service"),
+    serviceBaseUrl = "calendar.com",
+    name = "Calendar",
+    description = "This is the Calendar API",
+    context = ApiContext("calendar"),
+    versions = List(calendarApiVersion, calendarApiVersion2, calendarApiVersion3),
+    requiresTrust = false,
+    isTestSupport = false,
+    lastPublishedAt = None,
+    categories = List(ApiCategory.AGENTS)
+  )
+
 "retireApis" should {
     "fetch apis to retire and set them to retired" in new Setup {
+      when(mockAppConfig.apisToRetire).thenReturn(List("api1,2.0"))
+      print(calendarApiDefinition)
+      when(mockAPIDefinitionRepository.fetchByServiceName(*)).thenReturn(successful(Some(calendarApiDefinition)))
 
-      val result: String = await(underTest.SayHello())
-      result shouldBe "hellooo"
+      // val result: Unit = await(underTest.retireApis())
+      // calendarApiVersion2.status shouldBe(ApiStatus.RETIRED)
 
     }
   
-}
+  }
 
 }
