346aa03863294aad6bd7dd1d5625e4d04be86aee
jameshall1999 <67314983+jameshall1999@users.noreply.github.com>
2023-12-22 14:47:25 +0000

    API-7427 test WIP

--- app/uk/gov/hmrc/apidefinition/services/ApiRetirer.scala
+++ app/uk/gov/hmrc/apidefinition/services/ApiRetirer.scala
@@ -10,32 +10,40 @@ import uk.gov.hmrc.apidefinition.repository.APIDefinitionRepository
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.apiplatform.modules.apis.domain.models.ServiceName
 import uk.gov.hmrc.apiplatform.modules.common.domain.models.ApiVersionNbr
+import uk.gov.hmrc.apiplatform.modules.apis.domain.models.StoredApiDefinition
+import uk.gov.hmrc.apiplatform.modules.apis.domain.models.ApiStatus
+import scala.collection.mutable.ListBuffer
+import uk.gov.hmrc.apiplatform.modules.apis.domain.models.ApiVersion
 
 class ApiRetirer @Inject() (config: AppConfig, apiDefinitionRepository: APIDefinitionRepository)
     extends ApplicationLogger {
 
-  def retireApis()(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Unit] = {
+  def retireApis()(implicit ec: ExecutionContext): Future[Unit] = {
     logger.info(s"Attempting to retire ${config.apisToRetire.length} APIs.")
     Future.sequence(config.apisToRetire.map { api => findAndRetireApi(api) })
       .map(_ => ())
   }
 
-  private def findAndRetireApi(apiAndVersion: String)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Unit] = {
+  private def findAndRetireApi(apiAndVersion: String)(implicit ec: ExecutionContext): Future[Unit] = {
     val splitString = apiAndVersion.split(",")
     val api = splitString(0)
     val versionToRetire = splitString(1)
+    val listOfVersions = ListBuffer[ApiVersion]()
 
     apiDefinitionRepository.fetchByServiceName(ServiceName(api)) map {
       case Some(definition) => definition.versions.foreach {
         version => {
           if (version.versionNbr == ApiVersionNbr(versionToRetire)) {
-            version.status = ApiStatus.RETIRED 
+            val updatedVersion = version.copy(status = ApiStatus.RETIRED)
+            listOfVersions += updatedVersion
+          }
+          else {
+            listOfVersions += version
           }
         }
+        val updatedDefinition = definition.copy(versions = listOfVersions.toList)
+        apiDefinitionRepository.save(updatedDefinition)
       } 
     }
-
-    successful()
-    
   }
 }
--- test/uk/gov/hmrc/apidefinition/service/ApiRetirerSpec.scala
+++ test/uk/gov/hmrc/apidefinition/service/ApiRetirerSpec.scala
@@ -16,12 +16,14 @@ import uk.gov.hmrc.apiplatform.modules.apis.domain.models.Endpoint
 import uk.gov.hmrc.apiplatform.modules.apis.domain.models.HttpMethod
 import uk.gov.hmrc.apiplatform.modules.apis.domain.models.AuthType
 import uk.gov.hmrc.apiplatform.modules.apis.domain.models.ResourceThrottlingTier
+import uk.gov.hmrc.http.HeaderCarrier
 
 
 class ApiRetirerSpec extends AsyncHmrcSpec {
 
   trait Setup {
     implicit val ec: ExecutionContext = ExecutionContext.global
+    implicit val hc = HeaderCarrier()
 
     val mockAppConfig: AppConfig = mock[AppConfig]
     val mockAPIDefinitionRepository: APIDefinitionRepository = mock[APIDefinitionRepository]
@@ -62,6 +64,79 @@ class ApiRetirerSpec extends AsyncHmrcSpec {
     categories = List(ApiCategory.AGENTS)
   )
 
+  private val testApiDefinition2 = StoredApiDefinition(
+    serviceName = ServiceName("api2"),
+    serviceBaseUrl = "test.com",
+    name = "Test2",
+    description = "This is the Test API2",
+    context = ApiContext("test2"),
+    versions = List(testApiVersion1, testApiVersion2, testApiVersion3),
+    requiresTrust = false,
+    isTestSupport = false,
+    lastPublishedAt = None,
+    categories = List(ApiCategory.AGENTS)
+  )
+
+  private val expectedApiVersion1 = ApiVersion(
+    versionNbr = ApiVersionNbr("1.0"),
+    status = ApiStatus.RETIRED,
+    access = ApiAccess.PUBLIC,
+    endpoints = List(Endpoint("/date", "Check current date", HttpMethod.GET, AuthType.NONE, ResourceThrottlingTier.UNLIMITED))
+  )
+
+  private val expectedApiVersion2 = ApiVersion(
+    versionNbr = ApiVersionNbr("2.0"),
+    status = ApiStatus.RETIRED,
+    access = ApiAccess.PUBLIC,
+    endpoints = List(Endpoint("/date", "Check current date", HttpMethod.GET, AuthType.NONE, ResourceThrottlingTier.UNLIMITED))
+  )
+
+  private val expectedApiVersion3 = ApiVersion(
+    versionNbr = ApiVersionNbr("3.0"),
+    status = ApiStatus.RETIRED,
+    access = ApiAccess.PUBLIC,
+    endpoints = List(Endpoint("/date", "Check current date", HttpMethod.GET, AuthType.NONE, ResourceThrottlingTier.UNLIMITED))
+  )
+
+  private val expectedApiDefinition = StoredApiDefinition(
+    serviceName = ServiceName("api1"),
+    serviceBaseUrl = "test.com",
+    name = "Test",
+    description = "This is the Test API",
+    context = ApiContext("test"),
+    versions = List(testApiVersion1, expectedApiVersion2, testApiVersion3),
+    requiresTrust = false,
+    isTestSupport = false,
+    lastPublishedAt = None,
+    categories = List(ApiCategory.AGENTS)
+  )
+
+  private val expectedApiDefinition2 = StoredApiDefinition(
+    serviceName = ServiceName("api2"),
+    serviceBaseUrl = "test.com",
+    name = "Test2",
+    description = "This is the Test API2",
+    context = ApiContext("test2"),
+    versions = List(testApiVersion1, testApiVersion2, expectedApiVersion3),
+    requiresTrust = false,
+    isTestSupport = false,
+    lastPublishedAt = None,
+    categories = List(ApiCategory.AGENTS)
+  )
+
+  private val expectedApiDefinition3 = StoredApiDefinition(
+    serviceName = ServiceName("api2"),
+    serviceBaseUrl = "test.com",
+    name = "Test2",
+    description = "This is the Test API2",
+    context = ApiContext("test2"),
+    versions = List(expectedApiVersion1, testApiVersion2, testApiVersion3),
+    requiresTrust = false,
+    isTestSupport = false,
+    lastPublishedAt = None,
+    categories = List(ApiCategory.AGENTS)
+  )
+
 "retireApis" should {
     "fetch apis to retire and set them to retired" in new Setup {
       when(mockAppConfig.apisToRetire).thenReturn(List("api1,2.0"))
@@ -69,8 +144,21 @@ class ApiRetirerSpec extends AsyncHmrcSpec {
 
       await(underTest.retireApis())
       
-      val expectedApiDefinition
-      mockAPIDefinitionRepository.save(expectedApiDefinition)
+      verify(mockAPIDefinitionRepository, times(1)).save(expectedApiDefinition)
+    }
+  }
+
+"retireApis" should {
+    "fetch multiple apis and versions and set them to retired" in new Setup {
+      when(mockAppConfig.apisToRetire).thenReturn(List("api1,2.0", "api2,3.0", "api2,1.0"))
+      when(mockAPIDefinitionRepository.fetchByServiceName(ServiceName("api1"))).thenReturn(successful(Some(testApiDefinition)))
+      when(mockAPIDefinitionRepository.fetchByServiceName(ServiceName("api2"))).thenReturn(successful(Some(testApiDefinition2)))
+
+      await(underTest.retireApis())
+      
+      verify(mockAPIDefinitionRepository, times(1)).save(expectedApiDefinition)
+      verify(mockAPIDefinitionRepository, times(1)).save(expectedApiDefinition2)
+      verify(mockAPIDefinitionRepository, times(1)).save(expectedApiDefinition3)
     }
   }
 }
