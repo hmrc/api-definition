663c081cf76fe5e1ef28994c67e53fd31ee16190
John Green <john.green@digital.hmrc.gov.uk>
2026-03-25 07:44:56 +0000

    API-9031: Update mockito version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,9 +3,10 @@ import sbt._
 object AppDependencies {
   def apply(): Seq[ModuleID] = dependencies ++ testDependencies
 
-  private val bootstrapVersion = "10.7.0"
-  private val mongoPlayVersion = "2.12.0"
-  private val apiDomainVersion = "1.0.0"
+  private val bootstrapVersion    = "10.7.0"
+  private val mongoPlayVersion    = "2.12.0"
+  private val apiDomainVersion    = "1.0.0"
+  private val mockitoScalaVersion = "2.0.0"
 
   private lazy val dependencies = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-30" % bootstrapVersion,
@@ -16,7 +17,7 @@ object AppDependencies {
   private lazy val testDependencies = Seq(
     "uk.gov.hmrc"              %% "bootstrap-test-play-30"           % bootstrapVersion,
     "uk.gov.hmrc.mongo"        %% "hmrc-mongo-test-play-30"          % mongoPlayVersion,
-    "org.mockito"              %% "mockito-scala-scalatest"          % "1.17.37",
+    "org.mockito"              %% "mockito-scala-scalatest"          % mockitoScalaVersion,
     "uk.gov.hmrc"              %% "api-platform-api-domain-fixtures" % apiDomainVersion,
     "de.leanovate.play-mockws" %% "play-mockws-3-0"                  % "3.0.2"
   ).map(_ % "test")
